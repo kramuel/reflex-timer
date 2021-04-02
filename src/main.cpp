0d2a953bfb9a30b8964b568e17ad31b952562403
Samuel Karlström <sam.karlstrm@gmail.com>
2021-04-02 23:59:04 +0200

    it works!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
+#include <time.h>
 
-void num(int n)
+void show_num(int n)
 {
+  //PD1 = DP = 2
   //PD2 = B == 4(0100)
   //PD3 = C == 8(1000)
   //PD4 = D == 16()
@@ -10,7 +13,6 @@ void num(int n)
   //PD6 = A == 64
   //PD7 = F == 128
   //PB0 = G == 1
-  //PB1 = DP = 2
   switch (n)
   {
   case 0:
@@ -59,6 +61,10 @@ void num(int n)
     PORTD |= 64 + 128 + 4 + 8 + 16;
     PORTB |= 1;
     break;
+  case 10:
+    //G
+    PORTB |= 1;
+    break;
   default:
     break;
   }
@@ -67,16 +73,136 @@ void num(int n)
 void clear()
 {
   PORTD = 0;
-  PORTB &= (~1); //3?
+  PORTB &= (~1); //3? // 254? 0B11111110
+}
+
+void print_digits(int num1, int num2, int num3, int num4)
+{
+  int dig_delay = 1;
+  int loop_delay = 6;
+  //DIG1
+  PORTB = 0B00111000;
+  show_num(num1);
+  _delay_ms(dig_delay);
+  clear();
+
+  //DIG2
+  PORTB = 0B00110100;
+  PORTD |= 2;
+  show_num(num2);
+  _delay_ms(dig_delay);
+  clear();
+
+  //DIG3
+  PORTB = 0B00101100;
+  show_num(num3);
+  _delay_ms(dig_delay);
+  clear();
+
+  //DIG4
+  PORTB = 0B00011100;
+  show_num(num4);
+  _delay_ms(dig_delay);
+  clear();
+
+  _delay_ms(loop_delay);
+}
+
+void intro_loop()
+{
+  int r = 3 + (rand() % 7);
+  int counter = 0;
+  while (1)
+  {
+    // '-'
+    print_digits(10, 10, 10, 10);
+    counter++;
+    if (counter > 100 * r)
+      break;
+  }
+}
+
+void counter_loop(int *digits)
+{
+  int button = 0;
+  int buttonWasReleased = 0;
+
+  while (1)
+  {
+
+    button = PINB;
+
+    if (button & 1 << PB1)
+    {
+      if (buttonWasReleased == 1)
+      {
+        buttonWasReleased = 0;
+        break;
+      }
+    }
+    else
+    {
+      buttonWasReleased = 1;
+      digits[3]++;
+      if (digits[3] > 9)
+      {
+        digits[3] = 0;
+        digits[2]++;
+        if (digits[2] > 9)
+        {
+          digits[2] = 0;
+          digits[1]++;
+          if (digits[1] > 9)
+          {
+            digits[1] = 0;
+            digits[0]++;
+            if (digits[0] > 5)
+            {
+              digits[0] = 0;
+            }
+          }
+        }
+      }
+    }
+
+    print_digits(digits[0], digits[1], digits[2], digits[3]);
+  }
+}
+
+void show_result_loop(int *digits)
+{
+  int counter = 0;
+  while (1)
+  {
+    print_digits(digits[0], digits[1], digits[2], digits[3]);
+    counter++;
+    if (counter > 300)
+      break;
+  }
 }
 
 int main()
 {
-  DDRB = 0B00000011; //13.12.11.10 = input, 9.8 outputs
-  DDRD = 0B11111100; //7-3 outputs
+  //init
+  DDRB = 0B00111101; //13.12.11.10 = out, 9 in 8 G out
+  DDRD = 0B11111111; //outputs
+  int digits[4] = {0, 0, 0, 0};
+
+  srand(time(NULL));
 
   while (1)
   {
+    intro_loop();
+
+    counter_loop(digits);
+
+    show_result_loop(digits);
+
+    digits[0] = 0;
+    digits[1] = 0;
+    digits[2] = 0;
+    digits[3] = 0;
   }
+
   return 0;
 }
